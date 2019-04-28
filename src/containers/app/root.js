2e7ebf39e1cc5081d8d2a8ba65d2c9174f298301
John Yu <johnyu.programmer@gmail.com>
2019-04-28 14:17:36 -0400

    feat: initial commit, add new folder structure, add redux, base-ui

--- src/containers/app/root.js
+++ src/containers/app/root.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../../assets/logo.svg';
 
-function App() {
+function Root() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
+        <img src={logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -23,4 +21,4 @@ function App() {
   );
 }
 
-export default App;
+export default Root;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/index.css';
+import Root from './containers/app/root';
 import * as serviceWorker from './serviceWorker';
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 1. Create a client engine instance
+const engine = new Styletron();
+
+ReactDOM.render(<StyletronProvider value={engine}>
+  <Root />
+</StyletronProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
