2d4f5d26527624caee501e06dbd938c3b03b9df1
John Yu <johnyu.programmer@gmail.com>
2019-04-28 14:51:38 -0400

    feat: add navbar

--- src/containers/app/root.js
+++ src/containers/app/root.js
@@ -1,11 +1,32 @@
-import React from 'react';
+// TODO:
 
-function Root() {
-  return (
-    <div>
-    Hello World
-    </div>
-  );
-}
+// 1. Add holy-grail layout.
+// 2. Add holy-grail wrappers in pages.
+// 3. Add holy-grail components in here.
+
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import styled from 'styled-components';
+import { Switch, Route } from 'react-router-dom';
+
+// Components
+import NavBar from '../navbar';
+
+const AppWrapper = styled.div`
+  margin: 0 auto;
+  display: flex;
+  min-height: 100%;
+  padding: 0 0px;
+  flex-direction: column;
+`
+
+const Root = () => (
+  <AppWrapper>
+    <Helmet titleTemplate="%s - Fasting" defaultTitle="Fasting-Webapp">
+      <meta name="description" content="Fasting" />
+    </Helmet>
+    <NavBar />
+  </AppWrapper>
+)
 
 export default Root;
